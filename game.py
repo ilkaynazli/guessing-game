f5fec7d379d9bf2ff0178848490d8c8e6835a354
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 12:47:44 -0700

    working program

--- game.py
+++ game.py
@@ -4,3 +4,22 @@
 print ('hi player!')
 name = input("What's your name : ")
 print(name)
+
+import random
+
+secret_number = random.randint(1,100)
+guessed_count = 0
+
+while True:
+    guessed_count+=1
+    guess = int(input("Enter your guess "))
+    if guess == secret_number:
+        print("Yay")
+        break
+    elif guess < secret_number:
+        print("guess is too low")
+    else:
+        print("guess is too high")
+    
+
+print ("you have guessed {} times" .format(guessed_count))
