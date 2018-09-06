d9bdbd6bc2c5042af28bafefe44034b274d53fa1
vagrant <vagrant@vagrant.vm>
2018-09-06 05:45:09 +0000

    added range

--- game.py
+++ game.py
@@ -7,9 +7,16 @@ print ('Hello Player!')
 name = input("What's your name? ").title()
 print("Welcome {} to the number guessing game ".format(name))
 
+def get_range():
+    print ("Let's find out our range for the game.")
+    low_limit = int(input("Please enter the lower limit: "))
+    high_limit = int(input("Please enter the higher limit: "))
+    range_of_game = high_limit - low_limit
+    return (low_limit, high_limi, range_of_game)
 
 def guess_the_number():
-    secret_number = random.randint(1,100)
+    (low, high, best_score) = get_range()
+    secret_number = random.randint(low, high)
     guessed_count = 0
     limit = 15
     
@@ -21,7 +28,7 @@ def guess_the_number():
             print ("That is not an integer!")
             continue
 
-        if guess < 1 or guess > 100:
+        if guess < low or guess > high:
             print ('Your guess is out of range')
             continue
         guessed_count+=1
@@ -38,12 +45,12 @@ def guess_the_number():
         
 
     print ("You have guessed {} times" .format(guessed_count))
-    # if guessed_count < best_score:
-    #     best_score = guessed_count
-    #return best_score
+    if guessed_count < best_score:
+        best_score = guessed_count
+    return best_score
 
 
-guess_the_number()
+best_score = guess_the_number()
 
 
 
@@ -51,10 +58,10 @@ def choice():
     while True:
         choice_user = input("Would you like to play again? Y or N:").lower()
         if choice_user == 'y' or choice_user == 'yes':
-            guess_the_number()
+            best_score = guess_the_number()
         else:
             print('It was fun to play. See you again later')
-            #print("Your Best Score was {}".format(best_score))
+            print("Your Best Score was {}".format(best_score))
             break
 
 choice()
