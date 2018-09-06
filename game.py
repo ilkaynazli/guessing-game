778764d0607c5624b1cba813b4e5edd17ca861e7
vagrant <vagrant@vagrant.vm>
2018-09-06 05:31:17 +0000

    limit the guess count

--- game.py
+++ game.py
@@ -4,17 +4,17 @@
 # Put your code here
 import random
 print ('Hello Player!')
-name = input("What's your name : ").title()
-print("Welcome {} to the number guessing Game ".format(name))
+name = input("What's your name? ").title()
+print("Welcome {} to the number guessing game ".format(name))
 
 
 def guess_the_number():
     secret_number = random.randint(1,100)
     guessed_count = 0
-    best_score = 100
-
-    while True:
+    limit = 15
     
+    while True:
+
         try:    
             guess = int(input("Enter your guess "))
         except:
@@ -22,36 +22,39 @@ def guess_the_number():
             continue
 
         if guess < 1 or guess > 100:
-            print ('your guess is out of range')
+            print ('Your guess is out of range')
             continue
         guessed_count+=1
+        if guessed_count >= limit:
+            print ('You have guessed too many times. The game is over.')
+            break
         if guess == secret_number:
-            print("Yay, You Guessed Correct")
+            print("Yay, your guess is correct! Congratulations!")
             break
         elif guess < secret_number:
-            print("guess is too low")
+            print("Guess is too low.")
         else:
-            print("guess is too high")
+            print("Guess is too high.")
         
 
     print ("You have guessed {} times" .format(guessed_count))
-    if guessed_count < best_score:
-        best_score = guessed_count
-    return best_score
+    # if guessed_count < best_score:
+    #     best_score = guessed_count
+    #return best_score
 
 
-best_score = guess_the_number()
+guess_the_number()
 
 
 
 def choice():
     while True:
         choice_user = input("Would you like to play again? Y or N:").lower()
-        if choice_user == 'y':
+        if choice_user == 'y' or choice_user == 'yes':
             guess_the_number()
         else:
             print('It was fun to play. See you again later')
-            print("Your Best Score was {}".format(best_score))
+            #print("Your Best Score was {}".format(best_score))
             break
 
 choice()
